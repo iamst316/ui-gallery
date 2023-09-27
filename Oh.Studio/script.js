e527bf8a13e556bd8f2434a2c56346cbbeb74b73
Shubham Tandon <iamshubham316@gmail.com>
2023-09-27 19:46:52 +0530

    animated div on scroll

--- Oh.Studio/script.js
+++ Oh.Studio/script.js
@@ -21,3 +21,24 @@ function Select(num){
     // console.log(btn[num].innerText);
     // console.log(btn[num].classList);
 }
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+function handleScroll() {
+  const animatedDiv = document.getElementsByClassName('work')[0];
+  if (isInViewport(animatedDiv)) {
+    animatedDiv.classList.add('animate');
+    window.removeEventListener('scroll', handleScroll);
+  }
+}
+
+window.addEventListener('scroll', handleScroll);
+  
