3c90ace3f60f702a87e4ca1edbb7a10e68fe0972
Shubham Tandon <iamshubham316@gmail.com>
2023-12-04 13:59:08 +0530

    final faq-accordian

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -2,10 +2,10 @@
 const browserImg = "background-image: url(./assets/images/background-pattern-desktop.svg)"
 const mobileImg = "background-image: url(./assets/images/background-pattern-mobile.svg)"
 const faq = document.getElementsByClassName("faq")[0];
-const plusImg = "";
-const minusImg = "";
 const imgDiv = document.getElementsByClassName("bg-image")[0];
 const answers = document.getElementsByClassName("answer");
+const toggleBtn = document.getElementsByClassName("toggle-btn");
+
 
 if (window.innerWidth<=375){
   imgDiv.setAttribute("style",mobileImg)
@@ -18,14 +18,20 @@ else{
 
 
 }
-// console.log(answers);
+// console.log(toggleBtn[0].attributes.src);
 
 function toggle(idx){
-  console.log(answers)
   if (answers[idx].classList.contains("hidden")){
-    answers[idx].classList.replace("hidden","animate")
+    answers[idx].classList.replace("hidden","show")
+    toggleBtn[idx].attributes.src.value = "./assets/images/icon-minus.svg";
   }
-  else{
-    answers[idx].classList.replace("animate","hidden")
+  else if (answers[idx].classList.contains("hide")){
+    answers[idx].classList.replace("hide","show")
+    toggleBtn[idx].attributes.src.value = "./assets/images/icon-minus.svg";
+  }
+
+  else {
+    answers[idx].classList.replace("show","hide");
+    toggleBtn[idx].attributes.src.value = "./assets/images/icon-plus.svg";
   }
 }
